4b582e0f823ca847b7ee7336f744e04277b43b80
MatheusHenrique564 <125027622+Fervalinhos@users.noreply.github.com>
2026-01-31 20:41:48 -0300

    Criação do corpo da página baseada na VSat 4.0 , para a página de e-Pier com o erro 404. Instalação da biblioteca 'react-icons'

--- app/page.js
+++ app/page.js
@@ -1,11 +1,23 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { CiCalendar } from "react-icons/ci";
 
 export default function Home() {
   return (
     <main className={styles.main_page}>
       <div className={styles.page}>
-        <a>Landing Page</a>
+        <h1>e-Pier</h1>
+        <div>
+          <h2>a plataforma digital que integra</h2>
+          <h2>O seguro, agil e flexivel</h2>
+        </div>
+
+        <div>
+          <h3>Criar fluxos inteligentes com decisões baseadas em regras e dados operacionais;</h3>
+          <h3>Conceder autonomia às áreas de negócio, com total alinhamento à governança corporativa.</h3>
+        </div>
+
+        <button><CiCalendar />solicitar demonstração</button>
       </div>
     </main>
   );
