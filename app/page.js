9d89c5848a22e69a9f71d849bf9b903dea78042c
MatheusHenrique564 <125027622+Fervalinhos@users.noreply.github.com>
2026-01-31 21:42:43 -0300

    organizção dos containers e do css.module da landing page

--- app/page.js
+++ app/page.js
@@ -6,18 +6,27 @@ export default function Home() {
   return (
     <main className={styles.main_page}>
       <div className={styles.page}>
-        <h1>e-Pier</h1>
-        <div>
-          <h2>a plataforma digital que integra</h2>
-          <h2>O seguro, agil e flexivel</h2>
+
+        <div className={styles.information_container}>
+          <div className={styles.container_flex}>
+            <div className={styles.text_information_container}>
+              <h1>e-Pier</h1>
+              <h2>a plataforma digital que integra</h2>
+              <h2>O seguro, agil e flexivel</h2>
+                <h3>Criar fluxos inteligentes com decisões baseadas em regras e dados operacionais;</h3>
+              <h3>Conceder autonomia às áreas de negócio, com total alinhamento à governança corporativa.</h3>
+            </div>
+            <div className={styles.yt_container}></div>
+          </div>
+
+
+          <button className={styles.button}>
+            <CiCalendar className={styles.calendar_icon} />
+            <p>solicitar demonstração</p>
+          </button>
+
         </div>
 
-        <div>
-          <h3>Criar fluxos inteligentes com decisões baseadas em regras e dados operacionais;</h3>
-          <h3>Conceder autonomia às áreas de negócio, com total alinhamento à governança corporativa.</h3>
-        </div>
-
-        <button><CiCalendar />solicitar demonstração</button>
       </div>
     </main>
   );
