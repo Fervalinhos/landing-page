8f843f6e71be71065a4dab2b1c58c92ef451f99a
MatheusHenrique564 <125027622+Fervalinhos@users.noreply.github.com>
2026-01-31 22:52:22 -0300

    Fix: @media responsivo e biblioteca para videos no yt

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { CiCalendar } from "react-icons/ci";
+import ReactPlayer from 'react-player'
 
 export default function Home() {
   return (
@@ -10,13 +11,19 @@ export default function Home() {
         <div className={styles.information_container}>
           <div className={styles.container_flex}>
             <div className={styles.text_information_container}>
-              <h1>e-Pier</h1>
-              <h2>a plataforma digital que integra</h2>
-              <h2>O seguro, agil e flexivel</h2>
-                <h3>Criar fluxos inteligentes com decisões baseadas em regras e dados operacionais;</h3>
-              <h3>Conceder autonomia às áreas de negócio, com total alinhamento à governança corporativa.</h3>
+              <h1 className={styles.title}>e-Pier</h1>
+              <h2 className={styles.subtitle}>a plataforma digital que integra</h2>
+              <h2 className={styles.text_especial}>O seguro, agil e flexivel</h2>
+              <h3 className={styles.texts_information_container}>Fluxos inteligentes, decisões baseadas em regras e dados operacionais;</h3>
+              <h3 className={styles.texts_information_container}>Acessível nativamente pelo navegador.</h3>
             </div>
-            <div className={styles.yt_container}></div>
+            <div className={styles.yt_container}>
+              <ReactPlayer src='https://www.youtube.com/watch?v=7BWRS1zCyK4' style={{
+                width: "100%",
+                height: "100%",
+                "--controls": "none",
+              }} />
+            </div>
           </div>
 
 
