1420a1ab7f16be868438e034e95b2184c742ff02
MatheusHenrique564 <125027622+Fervalinhos@users.noreply.github.com>
2026-02-02 18:19:38 -0300

    Arrumando o css responsivo e criando o CRUD

---
+++ app/models/cadastro.js
@@ -0,0 +1,17 @@
+class Cadastro {
+    constructor( nome, telefone, email, cargo, nome_empresa, cnpj, setor, status, detalhe  ) {
+        this.id = id
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.cargo = cargo;
+        this.nome_empresa = nome_empresa;
+        this.cnpj = cnpj;
+        this.setor = setor;
+        this.status = status;
+        this.detalhe = detalhe
+        }
+
+}
+
+export default Cadastro;
--- app/page.jsx
+++ app/page.jsx
@@ -87,6 +87,10 @@ export default function Home() {
 
         </div>
 
+        <div>
+          
+        </div>
+
       </div>
     </main>
   );
